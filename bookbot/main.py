b975615ae7cbf3d0a8f4d483248a7de7c85e4f15
Stephanie <yesstephanie1@gmail.com>
2023-08-16 21:43:50 -0700

    count chars

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,23 +1,53 @@
+from collections import Counter
+
 
 def main():
     book_path = "books/frankenstein.txt"
-    # txt = get_book(book_path)
+    txt = get_book(book_path)
     split(book_path)
+    count_letters(book_path)
 
 
 def get_book(path):
     with open(path) as f:
         return f.read()
+
     
 def split(path):
     count = 0
-    with open(path) as txt:
-        words = txt.read()
-        splt = words.split()
+    words = get_book(path)
+    splt = words.split()
 
-        for _ in splt :
-            count += 1
+    for _ in splt :
+        count += 1
     print(count)
+
+def count_letters(path):
+    count_of_letters = {}
+    letter = get_book(path)
+  
+    letter = letter.lower()
+
+    count_of_letters  = Counter(letter)
+
+    print(count_of_letters)
+
+    for x, y in count_of_letters.items():
+        if x.isalpha:
+            print(f'The {x} character was found {y} times')
+    '''
+    my_dict = {}
+
+    for letter in words:
+        if letter in my_dict:
+            my_dict[letter] += 1
+        else:
+         # first entry
+            my_dict[letter] = 1
+    '''
+
+
+
             
 
 
