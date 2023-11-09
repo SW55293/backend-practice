eadc9cb89ee3c034e167d0a426f735513c3e1b57
Stephanie <yesstephanie1@gmail.com>
2023-11-08 22:24:11 -0800

    separating python and go

--- go/easy/fix_a_bug.go
+++ go/easy/fix_a_bug.go
@@ -8,12 +8,14 @@ func main() {
 		"Did you get my last message?",
 		"Don't leave me hanging...",
 		"Please respond I'm lonely!",
+		"Another line",
 	}
 	numMessages := float64(len(messagesFromDoris))
 	costPerMessage := .02
 
 	// don't touch above this line
 
+	// there was an arithmetic bug on line 17, just changed to *
 	totalCost := costPerMessage * numMessages
 
 	// don't touch below this line
