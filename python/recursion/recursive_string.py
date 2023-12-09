899de46272026b5eeb2c129e04cef41d580c3960
Stephanie <yesstephanie1@gmail.com>
2023-12-08 20:30:03 -0800

    python recursion

--- python/recursion/recursive_string.py
+++ python/recursion/recursive_string.py
@@ -7,7 +7,28 @@ the substrings inside. Your function should recurse once for each character in t
 '''
 
 def reverse_string(s):
+    # base case
     if len(s) == 0:
         return s
 
     return reverse_string(s[1:]) + s[0]
+
+# Example
+
+# Original String: "Hello"
+
+# Recursive Step 1:
+# reverse_string("ello") + "H"
+#   Recursive Step 2:
+#   reverse_string("llo") + "e"
+#     Recursive Step 3:
+#     reverse_string("lo") + "l"
+#       Recursive Step 4:
+#       reverse_string("o") + "l"
+#         Base Case: "o"
+#       Result: "o" + "l" = "ol"
+#     Result: "ol" + "l" = "oll"
+#   Result: "oll" + "e" = "olle"
+# Result: "olle" + "H" = "olleH"
+
+# Final Reversed String: "olleH"
