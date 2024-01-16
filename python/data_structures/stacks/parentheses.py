d750f85e1f2684394e1b17a0d8e358f446f08ee0
Stephanie <yesstephanie1@gmail.com>
2024-01-15 21:21:25 -0800

    stacks

---
+++ python/data_structures/stacks/parentheses.py
@@ -0,0 +1,23 @@
+'''
+ASSIGNMENT
+Write a function is_balanced(input_str) that takes a string input_str as input and returns True if the 
+parentheses in the string are balanced, and False otherwise. Use a stack to keep track of the parentheses.
+
+Note: You only need to consider the characters ( and ) for this challenge.
+
+TIP
+We've created a class named Stack with some helpful functions that you can use to complete the is_balanced function.
+
+
+'''
+
+def is_balanced(input_str):
+    stack = Stack()
+    
+    for char in input_str:
+        if char == "(":
+            stack.push(char)
+        elif char == ")":
+            if stack.pop() is None:
+                return False
+    return stack.peek() is None
